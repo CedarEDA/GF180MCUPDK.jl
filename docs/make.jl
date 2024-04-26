73848703e8bf732a3560b91085d4518a89ed5b6a
Fredrik Ekre <ekrefredrik@gmail.com>
2024-04-26 16:41:27 +0200

    Initial Buildkite configuration.

--- docs/make.jl
+++ docs/make.jl
@@ -1,29 +1,12 @@
 using Documenter
-using GF180MCUPDK
-
-# Normalize the documenter key if it's not already base64-encoded
-using Base64
-documenter_key = get(ENV, "DOCUMENTER_KEY", "")
-try
-    base64decode(documenter_key)
-catch e
-    if isa(e, ArgumentError)
-        if !endswith(documenter_key, "\n")
-            global documenter_key = string(documenter_key, "\n")
-        end
-        ENV["DOCUMENTER_KEY"] = base64encode(documenter_key)
-    else
-        rethrow(e)
-    end
-end
+# using GF180MCUPDK
 
 makedocs(
     sitename = "GF180MCUPDK",
     format = Documenter.HTML(),
-    modules = [GF180MCUPDK]
+    # modules = [GF180MCUPDK],
 )
 
 deploydocs(
     repo = "github.com/JuliaComputing/GF180MCUPDK.jl.git",
-    branch = "docs",
 )
